79297bbad8102ab648b172762ce835b72a20cd47
Th1nkK1D <witheep@gmail.com>
2023-01-23 00:44:35 +0200

    chore(scraper): Refactor post article element selector

--- facebook-scraper/src/actions.ts
+++ facebook-scraper/src/actions.ts
@@ -1,5 +1,7 @@
 import type { Page } from 'puppeteer';
 
+const GROUP_POST_ARTICLE_ELEMENT = '#m_group_stories_container article';
+
 export interface Post {
   publish_time: number;
   top_level_post_id: string;
@@ -27,51 +29,45 @@ export async function loginFacebook(
 }
 
 export async function getPostsFromGroupTimeline(page: Page): Promise<Post[]> {
-  return page.evaluate(() => {
+  return page.evaluate((articleSelector) => {
     const dataList: Post[] = [];
 
-    document
-      .querySelector('#m_group_stories_container')
-      ?.querySelectorAll('article')
-      .forEach((article) => {
-        const dataFt = article.getAttribute('data-ft');
+    document.querySelectorAll(articleSelector).forEach((article) => {
+      const dataFt = article.getAttribute('data-ft');
 
-        if (dataFt) {
-          const {
-            ent_attachement_type,
-            page_id,
-            page_insights,
+      if (dataFt) {
+        const {
+          ent_attachement_type,
+          page_id,
+          page_insights,
+          top_level_post_id,
+        } = JSON.parse(dataFt);
+
+        const publish_time = page_insights[page_id]?.post_context?.publish_time;
+
+        if (publish_time) {
+          const text = [...article.querySelectorAll('.story_body_container p')]
+            .map((p) => p.textContent)
+            .join('\n')
+            .replace('… More', '');
+
+          const postData = {
             top_level_post_id,
-          } = JSON.parse(dataFt);
+            ent_attachement_type,
+            actor_id: page_insights[page_id]?.actor_id,
+            publish_time: publish_time * 1000,
+            text,
+          };
 
-          const publish_time =
-            page_insights[page_id]?.post_context?.publish_time;
-
-          if (publish_time) {
-            const text = [
-              ...article.querySelectorAll('.story_body_container p'),
-            ]
-              .map((p) => p.textContent)
-              .join('\n')
-              .replace('… More', '');
-
-            const postData = {
-              top_level_post_id,
-              ent_attachement_type,
-              actor_id: page_insights[page_id]?.actor_id,
-              publish_time: publish_time * 1000,
-              text,
-            };
-
-            dataList.push(postData);
-          }
+          dataList.push(postData);
         }
+      }
 
-        article.remove();
-      });
+      article.remove();
+    });
 
     return dataList.sort((a, z) => z.publish_time - a.publish_time);
-  });
+  }, GROUP_POST_ARTICLE_ELEMENT);
 }
 
 export async function loadNextPostsFromTimeline(page: Page) {
@@ -80,5 +76,5 @@ export async function loadNextPostsFromTimeline(page: Page) {
     document.documentElement.scrollTop = document.documentElement.scrollHeight;
   });
 
-  await page.waitForSelector('#m_group_stories_container article');
+  await page.waitForSelector(GROUP_POST_ARTICLE_ELEMENT);
 }
