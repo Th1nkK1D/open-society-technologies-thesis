90b7d657a421466271d56a005d7a13744b4912ae
Th1nkK1D <witheep@gmail.com>
2023-04-14 17:55:25 +0300

    feat(dashboard): add topics grid

---
+++ dashboard/src/components/topics-grid.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import { Sentiment, type Post } from '../utils/posts';
+	import type { Topic } from '../utils/topics';
+
+	const SENTIMENT_COLORS = ['bg-green-500', 'bg-base-300', 'bg-red-500'];
+
+	export let topics: Topic[];
+	export let posts: Post[];
+
+	$: topicGroup = topics.map((topic) => {
+		const sentiments = Object.values(Sentiment).map(
+			(s) =>
+				posts.filter(({ topicId, sentiment }) => topicId === topic.id && sentiment === s).length
+		);
+
+		return {
+			...topic,
+			sentiments,
+			total: sentiments.reduce((sum, count) => sum + count, 0)
+		};
+	});
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+	{#each topicGroup as { id, label, color, total, sentiments }, index (id)}
+		{@const sentimentPercentages = sentiments.map((count) => (count * 100) / total)}
+
+		<div class="card bg-base-200 rounded border border-base-300 shadow">
+			<div class="card-body">
+				<span class="card-title" style="color: {color};">#{index + 1}</span>
+				<h2 class="card-title">
+					{label}
+				</h2>
+				<p>{total} posts</p>
+				<div class="flex flex-col text-xs space-y-1">
+					<div class="flex flex-row">
+						<span class="flex-1 text-green-500">{Math.round(sentimentPercentages[0])}%</span>
+						<span class="text-red-500">{Math.round(sentimentPercentages[2])}%</span>
+					</div>
+					<div class="flex flex-row bg-base-200 rounded-full h-2 overflow-hidden">
+						{#each sentimentPercentages as percentage, index}
+							<div class={SENTIMENT_COLORS[index]} style="width: {percentage}%" />
+						{/each}
+					</div>
+					<div class="flex flex-row text-gray-400">
+						<span class="flex-1">Positive</span>
+						<span>Negative</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	{/each}
+</div>
--- dashboard/src/routes/+page.svelte
+++ dashboard/src/routes/+page.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
 	import TopicsChart from '../components/topics-chart.svelte';
+	import TopicsGrid from '../components/topics-grid.svelte';
 	import { loadPosts } from '../utils/posts';
 	import { loadTopics } from '../utils/topics';
 
 	const MAX_DISPLAY_TOPIC = 20;
-
-	const loadData = Promise.all([loadPosts(), loadTopics()]);
 </script>
 
-<div>
-	{#await loadData}
-		Loading...
-	{:then [posts, allTopics]}
-		{@const topics = allTopics.slice(1, MAX_DISPLAY_TOPIC + 1)}
+{#await Promise.all([loadPosts(), loadTopics()])}
+	<progress class="progress w-full max-w-lg m-auto" />
+{:then [posts, allTopics]}
+	{@const topics = allTopics.slice(1, MAX_DISPLAY_TOPIC + 1)}
+
+	<div class="flex flex-col space-y-16">
+		<h1 class="font-sans font-bold text-3xl">
+			{MAX_DISPLAY_TOPIC} Most Discussed Topic by Expats in Estonia 2022
+		</h1>
 		<TopicsChart {posts} {topics} />
-	{/await}
-</div>
+		<TopicsGrid {posts} {topics} />
+	</div>
+{/await}
