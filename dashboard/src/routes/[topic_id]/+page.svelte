9b9c5aaf05ed91010c13ed0fbc8abfd367190f74
Th1nkK1D <witheep@gmail.com>
2023-04-15 16:08:37 +0300

    feat(dashboard): add topic conversation posts

--- dashboard/src/routes/[topic_id]/+page.svelte
+++ dashboard/src/routes/[topic_id]/+page.svelte
@@ -2,6 +2,13 @@
 	import { browser } from '$app/environment';
 	import SentimentsChart from '../../components/sentiments-chart.svelte';
 	import Wordcloud from '../../components/wordcloud.svelte';
+	import { Sentiment } from '../../models/post';
+
+	const SENTIMENT_COLORS = {
+		[Sentiment.Negative]: 'bg-red-500',
+		[Sentiment.Neutral]: 'bg-blue-500',
+		[Sentiment.Positive]: 'bg-green-500'
+	};
 
 	/** @type {import('./$types').PageData} */
 	export let data;
@@ -24,4 +31,23 @@
 	{#if browser}
 		<SentimentsChart {posts} />
 	{/if}
+</div>
+
+<div class="space-y-6">
+	<h2 class="text-xl"><span class="font-bold">Conversations</span> ({posts.length} posts)</h2>
+
+	<div class="flex flex-col space-y-4">
+		{#each posts as { text, timestamp, sentiment }}
+			<div class="card bg-base-200 rounded border border-base-300 shadow overflow-x-hidden">
+				<div class="card-body flex flex-col px-3 py-2">
+					<div class="flex flex-row text-sm">
+						<span class="badge text-xs text-white {SENTIMENT_COLORS[sentiment]}">{sentiment}</span>
+						<span class="flex-1" />
+						<span class="text-gray-600">{timestamp.toLocaleDateString()}</span>
+					</div>
+					<p>{text}</p>
+				</div>
+			</div>
+		{/each}
+	</div>
 </div>
