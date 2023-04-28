71604123192d115b93e40ad75fab5eb5585c0d12
Th1nkK1D <witheep@gmail.com>
2023-04-28 15:11:29 +0300

    feat(dashboard): add conversation keyword filter

--- dashboard/src/components/posts-feed.svelte
+++ dashboard/src/components/posts-feed.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import Mark from 'mark.js';
 	import { Sentiment, type Post, SENTIMENTS } from '../models/post';
 
 	const SENTIMENT_COLORS = {
@@ -9,18 +11,41 @@
 
 	export let posts: Post[];
 
+	let filterByKeyword = '';
 	let filterBySentiment: Sentiment | null = null;
+	let postsContainer: HTMLDivElement;
+	let keywordMarker: Mark;
 
-	$: filteredPost = filterBySentiment
-		? posts.filter(({ sentiment }) => sentiment === filterBySentiment)
-		: posts;
+	$: filteredPost = (
+		filterBySentiment ? posts.filter(({ sentiment }) => sentiment === filterBySentiment) : posts
+	).filter(({ text }) => text.includes(filterByKeyword));
+
+	$: {
+		if (postsContainer) {
+			keywordMarker?.unmark();
+
+			if (filterByKeyword.length > 1) {
+				keywordMarker = new Mark(postsContainer.querySelectorAll('p'));
+				keywordMarker.mark(filterByKeyword, { separateWordSearch: false });
+			}
+		}
+	}
 </script>
 
-<div class="space-y-6">
+<div class="space-y-6 flex flex-col">
+	<h2 class="text-xl flex-1">
+		<span class="font-bold">Conversations</span>
+	</h2>
+
 	<div class="flex flex-row items-center">
-		<h2 class="text-xl flex-1">
-			<span class="font-bold">Conversations</span>
-		</h2>
+		<input
+			type="text"
+			placeholder="Filter by a keyword..."
+			class="input input-bordered w-full max-w-md"
+			bind:value={filterByKeyword}
+		/>
+
+		<div class="flex-1" />
 
 		<div class="flex flex-row items-center justify-end space-x-4">
 			<span>Show</span>
@@ -35,7 +60,7 @@
 		</div>
 	</div>
 
-	<div class="flex flex-col space-y-4">
+	<div class="flex flex-col space-y-4" bind:this={postsContainer}>
 		{#each filteredPost as { text, timestamp, sentiment }}
 			<div class="card bg-base-200 rounded border border-base-300 shadow overflow-x-hidden">
 				<div class="card-body flex flex-col px-3 py-2">
